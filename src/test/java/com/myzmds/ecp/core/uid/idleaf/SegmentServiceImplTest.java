57d86466b43009948a90c94de81668d6c39d9133
linhuaichuan <linhuaichuan1989@126.com>
2019-03-06 16:44:18 +0800

    doc:uid::ecp-190306002 补充注释

--- src/test/java/com/myzmds/ecp/core/uid/idleaf/SegmentServiceImplTest.java
+++ src/test/java/com/myzmds/ecp/core/uid/idleaf/SegmentServiceImplTest.java
@@ -11,6 +11,21 @@ import com.myzmds.ecp.core.uid.leaf.SegmentServiceImpl;
 import net.sourceforge.groboutils.junit.v1.MultiThreadedTestRunner;
 import net.sourceforge.groboutils.junit.v1.TestRunnable;
 
+/**
+ * @类名称 SegmentServiceImplTest.java
+ * @类描述 <pre>Segment多线程并发测试</pre>
+ * @作者  庄梦蝶殇 linhuaichuan1989@126.com
+ * @创建时间 2019年3月6日 下午4:42:59
+ * @版本 1.0.0
+ *
+ * @修改记录
+ * <pre>
+ *     版本                       修改人 		修改日期 		 修改内容描述
+ *     ----------------------------------------------
+ *     1.0.0 	       庄梦蝶殇 	2019年3月6日             
+ *     ----------------------------------------------
+ * </pre>
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"file:src/test/resources/idleaf/app-leaf.xml"})
 public class SegmentServiceImplTest extends Thread {
@@ -27,7 +42,7 @@ public class SegmentServiceImplTest extends Thread {
                 System.out.println(Thread.currentThread().getName() + ":" + segmentServiceImpl.getId());
             }
         };
-        // 开13个线程，模拟13个用户
+        // 开13，23，43个线程进行测试,step设置为10
         int runnerCount = 23;
         TestRunnable[] trs = new TestRunnable[runnerCount];
         for (int i = 0; i < runnerCount; i++) {
